e03fa011d302a5202cce975f02059fe4a4c2214d
Ranjit Parmar <parmarranjit7972@gmail.com>
2024-09-28 02:18:51 +0530

    all required changes applied

--- src/components/Spinner/Spinner.jsx
+++ src/components/Spinner/Spinner.jsx
@@ -3,7 +3,7 @@ import { BeatLoader } from 'react-spinners'
 
 const Spinner = () => {
   return (
-    <div className='flex justify-center items-center border-black border-2'>
+    <div className='flex justify-center items-center'>
       <BeatLoader style={{ 'height': 'calc(100vh - 1.75rem)', 'lineHeight': 'calc(100vh - 1.75rem) ' }} size={23} color={'#fa7e0a'} />
     </div>
   )
