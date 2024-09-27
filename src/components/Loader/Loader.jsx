c000abd00f89aed09c1a10c8088aafa7a7980680
Ranjit Parmar <parmarranjit7972@gmail.com>
2024-09-28 02:52:31 +0530

    all required changes applied

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -3,8 +3,8 @@ import { FadeLoader } from 'react-spinners'
 
 const Loader = () => {
   return (
-    <div className='flex justify-center items-center'>
-      <FadeLoader style={{ 'height': 'calc(100vh - 1.75rem)', 'textAlign':'center', 'lineHeight': 'calc(100vh - 1.75rem) ' }} size={23} color={'#fa7e0a'} />
+    <div className='flex justify-center items-center h-full w-full'>
+      <FadeLoader style={{ 'height': 'calc(100vh - 1.75rem)', 'lineHeight': 'calc(100vh - 1.75rem) ' }} size={23} color={'#fa7e0a'} />
     </div>  
   )
 }
