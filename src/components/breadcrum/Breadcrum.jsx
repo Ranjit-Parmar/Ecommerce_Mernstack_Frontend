2263a56117119800bfdec619156906af6a33fe36
Ranjit Parmar <parmarranjit7972@gmail.com>
2024-10-01 01:41:37 +0530

    all required changes applied

--- src/components/breadcrum/Breadcrum.jsx
+++ src/components/breadcrum/Breadcrum.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import arrow from '../../assets/breadcrum_arrow.png';
 
 
 const Breadcrum = ({category, gender}) => {
   return (
     <div className='flex text-xs lg:text-base items-center gap-2 font-normal my-[30px] capitalize pl-4'>
-        HOME <img src="../../src/assets/breadcrum_arrow.png" alt="" /> SHOP <img src="../../src/assets/breadcrum_arrow.png" alt="" /> {gender.toUpperCase()} <img src="../../src/assets/breadcrum_arrow.png" alt="" /> {category.toUpperCase()}
+        HOME <img src={arrow} alt="" /> SHOP <img src={arrow} alt="" /> {gender==='male'?MEN:gender==='female'?WOMEN:KID} <img src={arrow} alt="" /> {category.toUpperCase()}
     </div>
   )
 }
