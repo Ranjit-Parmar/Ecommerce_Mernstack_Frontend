8e1a9101ef903246936692241190cb325c3f3cba
Ranjit Parmar <parmarranjit7972@gmail.com>
2024-10-11 21:04:53 +0530

    changes applied

--- src/components/protectedRoute/ProtectedRoute.jsx
+++ src/components/protectedRoute/ProtectedRoute.jsx
@@ -18,12 +18,12 @@ const ProtectedRoute = ({ children }) => {
   
   useEffect(()=>{
 
-    if(userdata.status === 401){
+    if(userdata[0]?.status === 401){
         dispatch(logOutUser());
         toast.error("please login again")
         return <Navigate to="/login" replace={true}/>
     }
-      dispatch(logInUser(userdata))
+      dispatch(logInUser(userdata[0]))
 
     if(data?.cartItem){
       dispatch(fetchCartItems(data?.cartItem))
@@ -32,7 +32,7 @@ const ProtectedRoute = ({ children }) => {
 
   },[userdata,dispatch,children,isLoggedInUser,data,isLoading])
 
-  if(userdata || isLoggedInUser){
+  if(userdata[0] || isLoggedInUser){
     return children;
   }
 
