1d827a2bf5c93b9ac18726fad3d3441a71efcd88
Ranjit Parmar <parmarranjit7972@gmail.com>
2024-10-11 20:42:13 +0530

    changes applied

--- src/components/protectedRoute/ProtectedRoute.jsx
+++ src/components/protectedRoute/ProtectedRoute.jsx
@@ -2,15 +2,12 @@ import { Navigate, useLoaderData } from 'react-router-dom';
 import { fetchCartItems } from '../../redux/reducers/cartReducer';
 import { useDispatch, useSelector } from 'react-redux';
 import { logInUser, logOutUser } from '../../redux/reducers/userReducer';
-import { useEffect, useState } from 'react';
-import { useMyCartItemsQuery } from '../../redux/Api/cartApi';
+import { useEffect } from 'react';
 import toast from "react-hot-toast";
 
 
 const ProtectedRoute = ({ children }) => { 
 
-  const {data, isLoading, isError} = useMyCartItemsQuery();
-  const [cartData, setCartData] = useState(data?.cartItem?data.cartItem:[]);
   const {isLoggedInUser} = useSelector((state)=>state.userReducer);
   const dispatch = useDispatch();
   const userdata = useLoaderData();
@@ -18,21 +15,17 @@ const ProtectedRoute = ({ children }) => {
   
   useEffect(()=>{
     
-    if(userdata){
-      dispatch(logInUser(userdata))
-      dispatch(fetchCartItems(cartData))
+    if(userdata[0]?.status === 401){
+        dispatch(logOutUser());
+        toast.error("please login again")
+        return <Navigate to="/login" replace={true}/>
     }
-    dispatch(logOutUser());
-    toast.error("please login again")
-    return <Navigate to="/login" replace={true}/>
-    
-    
+      dispatch(logInUser(userdata[0]))
+      dispatch(fetchCartItems(userdata[1]))
 
   },[userdata,dispatch,children,isLoggedInUser])
 
-
-
-  if(userdata || isLoggedInUser){
+  if(userdata[0] || isLoggedInUser){
     return children;
   }
 
