bc8f472454fa06165f5c60726968dc110271cfaf
Ranjit Parmar <parmarranjit7972@gmail.com>
2024-10-13 02:02:58 +0530

    changes applied

--- src/components/protectedRoute/ProtectedRoute.jsx
+++ src/components/protectedRoute/ProtectedRoute.jsx
@@ -2,27 +2,37 @@ import { Navigate, useLoaderData } from 'react-router-dom';
 import { fetchCartItems } from '../../redux/reducers/cartReducer';
 import { useDispatch, useSelector } from 'react-redux';
 import { loadUser, logInUser, logOutUser } from '../../redux/reducers/userReducer';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import toast from 'react-hot-toast';
 import { useLoadUserQuery } from '../../redux/Api/userApi';
+
+
 
 const loadUserData = async () => {
   const userData = await loadUser();
   return userData;
 }
-loadUserData().then((a)=>{
-  console.log(a);
-}).catch((e)=>{
-  console.log(e);
-  
-});
 
 
 
 const ProtectedRoute = ({ children }) => { 
 
+  const [userData, setUserData] = useState(null);
 
- 
+  useEffect(()=>{
+    loadUserData().then((a)=>{
+      setUserData(a)
+    }).catch((e)=>{
+      console.log(e); 
+    });
+  },[])
+
+  if(userData){
+    return children;
+  }else{
+    return <Navigate to='/login' replace={true}/>
+  }
+
 }
 
 export default ProtectedRoute;
