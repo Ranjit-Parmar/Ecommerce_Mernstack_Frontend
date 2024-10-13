fe98dad0fbaf719c8459bf230ced3e6f6fe81e3b
Ranjit Parmar <parmarranjit7972@gmail.com>
2024-10-13 16:28:52 +0530

    changes applied

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client';
 import Store from './store/Store.js';
 import ShopContextProvider from './context/ShopContext.jsx'
 import { Provider } from 'react-redux';
+import Routes from './routes/Routes.jsx';
 
 
 
@@ -14,7 +15,7 @@ import { Provider } from 'react-redux';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={Store}>
     <ShopContextProvider>
-      
+      <Routes/>
     </ShopContextProvider>
   </Provider>
 )
